8ec7d76401c6b8be33c352856b0a7ba5cc305e05
Adam Flott <adam@npjh.com>
2012-01-21 17:19:49 -0500

    Emacs: fix language loading issue for en_US on OSX

--- editors/emacs/prelude/modules/prelude-editor.el
+++ editors/emacs/prelude/modules/prelude-editor.el
@@ -143,6 +143,14 @@
 ;; flyspell-mode does spell-checking on the fly as you type
 (setq ispell-program-name "aspell" ; use aspell instead of ispell
       ispell-extra-args '("--sug-mode=ultra"))
+
+(if (eq system-type 'darwin)
+     (if (file-executable-p "/usr/local/bin/aspell")
+     (progn
+       (setq ispell-program-name "/usr/local/bin/aspell")
+       (setq ispell-extra-args '("-d" "/Library/Application Support/cocoAspell/aspell6-en-6.0-0/en.multi"))
+       )))
+
 (autoload 'flyspell-mode "flyspell" "On-the-fly spelling checker." t)
 
 (defun prelude-turn-on-flyspell ()
